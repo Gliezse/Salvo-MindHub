edaa7d8d0cc0d2056d2fbce5f0b959933766699a
Gliezse <cristiancruz2000@hotmail.com.ar>
2019-06-27 14:29:53 -0300

    Deleted unused imports

--- src/main/java/com/codeoftheweb/Salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/Salvo/SalvoController.java
@@ -1,7 +1,5 @@
 package com.codeoftheweb.Salvo;
 
-import org.apache.coyote.Response;
-import org.aspectj.apache.bcel.util.Play;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
